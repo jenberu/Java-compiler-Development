f41c82dbd5da7b8d6a0c977cf1ef0dffc0586d36
jenberu <jemberu0970@gmail.com>
2024-03-24 14:34:48 +0300

    the first stage (lexical) to develop java compiler

---
+++ input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = scanner.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Even number.");
+        } else {
+            System.out.println("Odd number.");
+        }
+        scanner.close();
+    }
+}
